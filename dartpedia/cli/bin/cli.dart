7360bec1e8ce85f768b31bda75737fe010f946ac
Dunaeva Alena <dunalalex1@gmail.com>
2026-04-01 22:30:28 +0300

    Обновленный код с поиском из википедии searchWikipedia

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -2,33 +2,41 @@ import 'dart:io';
 import 'package:http/http.dart' as http; 
 const version = '0.0.1';
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
+if (arguments.isEmpty || arguments.first == 'help') {
     printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'search') {
+  } else if (arguments.first == 'wikipedia') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searchWikipedia(inputArgs);
+    searchWikipedia(inputArgs); 
   } else {
-    printUsage();
+    printUsage(); 
   }
-}
-void searchWikipedia(List<String>? arguments) { 
-  final String articleTitle;
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    articleTitle = stdin.readLineSync() ?? '';
-  } else {
-    articleTitle = arguments.join(' ');
-  }
-  print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
 }
 void printUsage() { 
   print(
     "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
+}
+void searchWikipedia(List<String>? arguments) async {
+  final String articleTitle;
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    final inputFromStdin = stdin.readLineSync();
+    if (inputFromStdin == null || inputFromStdin.isEmpty) {
+      print('No article title provided. Exiting.');
+      return;
+    }
+    articleTitle = inputFromStdin;
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+
+
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent); 
 }
 
 Future<String> getWikipediaArticle(String articleTitle) async {
