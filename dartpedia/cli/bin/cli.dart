be318b7c9d830211868008969ca4cdc9e2c1f65a
Dunaeva Alena <dunalalex1@gmail.com>
2026-04-01 21:48:51 +0300

    Реализация команды поиска

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -2,12 +2,18 @@ const version = '0.0.1';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); 
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
   } else {
-    printUsage(); 
+    printUsage();
   }
+}
+void searchWikipedia(List<String>? arguments) { 
+  print('searchWikipedia received arguments: $arguments');
 }
 void printUsage() { 
   print(
