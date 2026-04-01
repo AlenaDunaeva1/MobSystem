242a3eda2dc81f9f685a2a1e23cb18874fcb3320
Dunaeva Alena <dunalalex1@gmail.com>
2026-04-01 21:12:16 +0300

    Реализация команды version и printUsage

---
+++ dartpedia/cli/bin/cli.dart
@@ -0,0 +1,14 @@
+const version = '0.0.1'; 
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Hello, Dart!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  }
+}
+void printUsage() { // Add this new function
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
