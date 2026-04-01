17f822a186114de9ed3401c9a0144a641ee9fad7
Dunaeva Alena <dunalalex1@gmail.com>
2026-04-02 00:58:02 +0300

    Улучшен результат в HelpCommand, добавлена функцияon Outputобратного вызова, обновлено основное приложение cli.dart

--- dartpedia/command_runner/lib/src/command_runner_base.dart
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -5,21 +5,25 @@ import 'arguments.dart';
 import 'exceptions.dart'; 
 
 class CommandRunner {
-  CommandRunner({this.onError});
+  CommandRunner({this.onOutput, this.onError});
   final Map<String, Command> _commands = <String, Command>{};
 
   UnmodifiableSetView<Command> get commands =>
       UnmodifiableSetView<Command>(<Command>{..._commands.values});
-
+  FutureOr<void> Function(String)? onOutput;
   FutureOr<void> Function(Object)? onError;
   Future<void> run(List<String> input) async {
     try {
       final ArgResults results = parse(input);
       if (results.command != null) {
         Object? output = await results.command!.run(results);
-        print(output.toString());
+        if (onOutput != null) {
+          await onOutput!(output.toString());
+        } else {
+          print(output.toString());
+        }
       }
-    }on Exception catch (exception) {
+    } on Exception catch (exception) {
       if (onError != null) {
         onError!(exception);
       } else {
